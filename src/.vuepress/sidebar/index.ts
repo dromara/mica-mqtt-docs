8a952678eafd70cc708295a8defd9492f0fb95ba
如梦技术 <596392912@qq.com>
2024-12-12 10:53:38 +0800

    完善 mica-mqtt 文档

--- src/.vuepress/sidebar/index.ts
+++ src/.vuepress/sidebar/index.ts
@@ -13,7 +13,32 @@ export const zhSidebar = sidebar({
       text: "mica-mqtt指南",
       icon: "lightbulb",
       prefix: "guide/",
-      children: "structure",
+      children: [
+        {
+          text: "普通 Java 应用",
+          icon: "java",
+          prefix: "java/",
+          children: ["client", "server"],
+        },
+        {
+          text: "Spring boot 应用",
+          icon: "java",
+          prefix: "spring/",
+          children: ["client", "server"],
+        },
+        {
+          text: "Solon 应用",
+          icon: "java",
+          prefix: "solon/",
+          children: ["client", "server"],
+        },
+        {
+          text: "JFinal 应用",
+          icon: "java",
+          prefix: "jfinal/",
+          children: ["client", "server"],
+        },
+      ],
     }
   ],
 });
